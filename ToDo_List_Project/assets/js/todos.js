2f00bbe5c67a5235ad6e701030435ba156d10de1
Yongzheng Zhang <yongzheng9405@gmail.com>
2019-03-31 15:44:24 +0800

    Creating ToDos

--- ToDo_List_Project/assets/js/todos.js
+++ ToDo_List_Project/assets/js/todos.js
@@ -1,14 +1,27 @@
 // Check off specific to-do when clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("complete");
 });
 
 //Click X to delete to-do
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	//avoid the event bubble
 	event.stopPropagation();
 
+});
+
+//Add to-do
+$("input[type='text']").keypress(function(event){
+	//the enter event number is 13
+	if(event.which === 13) {
+		//grabbing new to-do text from the input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText +"</li>")
+
+	}
 });
